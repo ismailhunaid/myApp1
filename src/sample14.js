230f7c78e801320aeb8b59f9b32858fee35894cc
ismailhunaid <ismael552@gmail.com>
2020-07-25 01:18:39 +0500

    cssworking

--- src/sample14.js
+++ src/sample14.js
@@ -1,15 +1,26 @@
 import React from'react';
+import './sample14.css'
 
 const Sample14 = () => {
     return (
-        <div>
-            <h1> hello ismail </h1>
-            <table>
+        <div >
+            <h1 className = "try"> hello ismail </h1>
+                    <table>
                 <tr> fruits</tr>
                 <td> mango</td>
                 <td> banana</td>
             </table>
-        </div>
+            <table>
+                <tr>
+                    cars
+                </tr>
+                <td> cultus</td>
+                <td> honda</td>
+            </table>
+
+            </div>
+            
+        
         
     )
 }
